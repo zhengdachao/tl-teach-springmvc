51ffb4085e02e2ac7de3332b7e9b8c8064761d0b
zhengdachao <610477202@qq.com>
2018-08-25 11:27:16 +0800

    修改了更新的功能

--- src/main/java/com/tl/UserController.java
+++ src/main/java/com/tl/UserController.java
@@ -4,20 +4,22 @@ package com.tl;
  * Created by 郑大朝 on 2018/5/19.
  */
 public class UserController {
-    /**
-     * @param args
-     * @Description TODO(高秀玲的账号)
-     */
-    public static void main(String[] args) {
+	/**
+	 * @param args
+	 * @Description TODO(高秀玲的账号)
+	 */
+	public static void main(String[] args) {
 
-        System.out.println("I Miss You");
+		System.out.println("I Miss You");
 
-    }
+	}
 
-    /**
-     * 添加更新的方法
-     */
-    public void updateUser() {
-        System.out.println("更新用户");
-    }
+	/**
+	 * 添加更新的方法
+	 */
+	public void updateUser() {
+		System.out.println("更新用户的功能");
+		// 逻辑操作
+		// 未完成的功能
+	}
 }
